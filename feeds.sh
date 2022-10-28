7cc9e6c78817b9c472c1d1c51b7e45360876c4df
coolkid <s196774065@gmail.com>
2022-10-29 01:53:53 +0800

    update feeds

--- feeds.sh
+++ feeds.sh
@@ -1,4 +1,9 @@
 ./scripts/feeds update -a
 rm -rf ./feeds/luci/applications/luci-app-passwall
+rm -rf ./feeds/luci/applications/luci-app-smartdns
+rm -rf ./feeds/package/net/smartdns
+
 ./scripts/feeds install -a
 ./scripts/feeds install -p diy2 luci-app-passwall
+./scripts/feeds install -p custom luci-app-smartdns
+./scripts/feeds install -p custom smartdns
