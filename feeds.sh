fc9090993a0db85d1f96b16376783507980887e7
coolkid <s196774065@gmail.com>
2023-02-02 00:12:41 +0800

    update feeds.sh

--- feeds.sh
+++ feeds.sh
@@ -1,5 +1,6 @@
 ./scripts/feeds update -a
 rm -rf ./feeds/luci/applications/luci-app-passwall
+rm -rf ./feeds/packages/net/mosdns/
 
 ./scripts/feeds install -a
 ./scripts/feeds install -p diy2 luci-app-passwall
