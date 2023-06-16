023307fe5d7a1a2bde793c6c20d4a0c45a32f57f
Coolkids <s196774065@gmail.com>
2023-06-16 22:44:48 +0800

    修改包

--- tool.sh
+++ tool.sh
@@ -7,14 +7,6 @@ case $1 in
 	feed
 	echo "update feed finish"
 	;;
-"ip1")
-	switch1
-	echo "switch iptables 1.8.7"
-	;;
-"ip2")
-	switch2
-	echo "switch iptables 1.8.8"
-	;;
 *)
 	echo "param invalid"
 	;;
@@ -24,24 +16,10 @@ esac
 function feed(){
 ./scripts/feeds update -a
 rm -rf ./feeds/luci/applications/luci-app-passwall
-rm -rf ./feeds/luci/applications/luci-app-smartdns
 rm -rf ./feeds/packages/net/mosdns
-rm -rf ./feeds/packages/net/smartdns
 ./scripts/feeds install -a
-./scripts/feeds install -p custom smartdns
-./scripts/feeds install -p custom luci-app-smartdns
 ./scripts/feeds install -p diy2 luci-app-passwall
 ./scripts/feeds install -p mosdns mosdns-v5
-}
-
-function switch1(){
-rm -rf package/network/utils/iptables/
-cp -r ../iptables package/network/utils/iptables
-}
-
-function switch2(){
-rm -rf package/network/utils/iptables/
-cp -r ../iptables2 package/network/utils/iptables
 }
 
 main $1
