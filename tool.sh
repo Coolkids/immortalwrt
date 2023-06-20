b8f67b1b2ecd867e615bb5ad6fdb525ef5e5f52f
Coolkids <s196774065@gmail.com>
2023-06-20 19:04:08 +0800

    [skip ci]清理不需要的feed

--- tool.sh
+++ tool.sh
@@ -14,12 +14,11 @@ esac
 }
 
 function feed(){
+rm -rf ./feeds/*
 ./scripts/feeds update -a
 rm -rf ./feeds/luci/applications/luci-app-passwall
-rm -rf ./feeds/packages/net/mosdns
 ./scripts/feeds install -a
 ./scripts/feeds install -p diy2 luci-app-passwall
-./scripts/feeds install -p mosdns mosdns-v5
 cd ./feeds/luci
 git apply ../../.github/patch/001-luci-status-network-ifaces.patch
 }
