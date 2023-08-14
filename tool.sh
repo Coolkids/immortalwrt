05f75841fbb3dfaa226bbd17c3f70315ba09e354
Coolkids <s196774065@gmail.com>
2023-08-14 23:48:28 +0800

    [skip ci]更新包

--- tool.sh
+++ tool.sh
@@ -17,8 +17,12 @@ function feed(){
 rm -rf ./feeds/*
 ./scripts/feeds update -a
 rm -rf ./feeds/luci/applications/luci-app-passwall
+rm -rf ./feeds/packages/net/v2ray-geodata
+rm -rf ./feeds/packages/net/mosdns
 ./scripts/feeds install -a
 ./scripts/feeds install -p diy2 luci-app-passwall
+./scripts/feeds install -p custom v2ray-geodata
+./scripts/feeds install -p mosdns mosdns
 cd ./feeds/luci
 git apply ../../.github/patch/001-luci-status-network-ifaces.patch
 }
