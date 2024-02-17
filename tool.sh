4850d5c686c46bb9f83d7054977125e488785aba
Coolkids <s196774065@gmail.com>
2024-02-17 12:35:16 +0800

    添加openssl quic补丁，更新ci

--- tool.sh
+++ tool.sh
@@ -19,14 +19,20 @@ rm -rf ./feeds/*
 rm -rf ./feeds/luci/applications/luci-app-passwall
 rm -rf ./feeds/packages/net/v2ray-geodata
 rm -rf ./feeds/packages/net/mosdns
-rm -rf ./feeds/packages/net/nlbwmon
 ./scripts/feeds install -a
 ./scripts/feeds install -p diy2 luci-app-passwall
 ./scripts/feeds install -p custom v2ray-geodata
-./scripts/feeds install -p custom nlbwmon
 ./scripts/feeds install -p mosdns mosdns
-cd ./feeds/luci
-git apply ../../.github/patch/001-luci-status-network-ifaces.patch
+pushd ./feeds/luci
+git apply ./patchs/001-luci-status-network-ifaces.patch
+popd
+
+rm -rf ./feeds/packages/libs/nghttp3
+rm -rf ./feeds/packages/libs/ngtcp2
+
+cp -r ./patchs/ngtcp2 ./feeds/packages/libs
+cp -r ./patchs/nghttp3 ./feeds/packages/libs
+
 }
 
 main $1
