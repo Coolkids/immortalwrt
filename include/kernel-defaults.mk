9057566082c99009cd36f396b0f55b94466883b1
Coolkids <s196774065@gmail.com>
2024-04-25 14:56:29 +0800

    [skip ci] 添加6.6补丁

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -104,6 +104,7 @@ define Kernel/Configure/Default
 	$(LINUX_CONF_CMD) > $(LINUX_DIR)/.config.target
 # copy CONFIG_KERNEL_* settings over to .config.target
 	awk '/^(#[[:space:]]+)?CONFIG_KERNEL/{sub("CONFIG_KERNEL_","CONFIG_");print}' $(TOPDIR)/.config >> $(LINUX_DIR)/.config.target
+	## 开启livepath功能
 	##echo "# CONFIG_KALLSYMS_EXTRA_PASS is not set" >> $(LINUX_DIR)/.config.target
 	##echo "# CONFIG_KALLSYMS_ALL is not set" >> $(LINUX_DIR)/.config.target
 	##echo "CONFIG_KALLSYMS_UNCOMPRESSED=y" >> $(LINUX_DIR)/.config.target
