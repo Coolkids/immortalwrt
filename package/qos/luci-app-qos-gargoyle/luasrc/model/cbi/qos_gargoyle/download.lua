e3bb1bde2a99fbe33ef6ef35a5c3bd21996ad9cb
coolkid <s196774065@gmail.com>
2024-12-25 10:01:44 +0800

    更新luci-app-qos-gargoyle

--- package/qos/luci-app-qos-gargoyle/luasrc/model/cbi/qos_gargoyle/download.lua
+++ package/qos/luci-app-qos-gargoyle/luasrc/model/cbi/qos_gargoyle/download.lua
@@ -6,7 +6,6 @@ local uci  = require "luci.model.uci".cursor()
 local dsp  = require "luci.dispatcher"
 local http = require "luci.http"
 local qos  = require "luci.model.qos_gargoyle"
-local i18n = require "luci.i18n"
 
 local m, class_s, rule_s, o
 local download_classes = {}
@@ -36,11 +35,11 @@ uci:foreach(qos_gargoyle, "download_rule", function(s)
 	end
 end)
 
-m = Map(qos_gargoyle, i18n.translate("Download Settings"))
+m = Map(qos_gargoyle, translate("Download Settings"))
 m.template = "qos_gargoyle/list_view"
 
-class_s = m:section(TypedSection, "download_class", i18n.translate("Service Classes"),
-	i18n.translate("Each service class is specified by four parameters: percent bandwidth at <br />capacity, realtime bandwidth and maximum bandwidth and the minimimze round <br />trip time flag.")
+class_s = m:section(TypedSection, "download_class", translate("Service Classes"),
+	translate("Each service class is specified by four parameters: percent bandwidth at <br />capacity, realtime bandwidth and maximum bandwidth and the minimimze round <br />trip time flag.")
 	)
 class_s.anonymous = true
 class_s.addremove = true
@@ -53,44 +52,44 @@ class_s.create    = function(e, t)
 	http.redirect(class_s.extedit:format(t))
 end
 
-o = class_s:option(DummyValue, "name", i18n.translate("Class Name"))
+o = class_s:option(DummyValue, "name", translate("Class Name"))
 o.cfgvalue = function(...)
-	return Value.cfgvalue(...) or i18n.translate("None")
+	return Value.cfgvalue(...) or translate("None")
 end
 
-o = class_s:option(DummyValue, "percent_bandwidth", i18n.translate("Percent Bandwidth At Capacity"))
+o = class_s:option(DummyValue, "percent_bandwidth", translate("Percent Bandwidth At Capacity"))
 o.cfgvalue = function(...)
 	local v = tonumber(Value.cfgvalue(...))
 	if v and v > 0 then
 		return "%d %%" % v
 	end
-	return i18n.translate("Not set")
+	return translate("Not set")
 end
 
-o = class_s:option(DummyValue, "min_bandwidth", "%s (kbps)" % i18n.translate("Minimum Bandwidth"))
+o = class_s:option(DummyValue, "min_bandwidth", "%s (kbps)" % translate("Minimum Bandwidth"))
 o.cfgvalue = function(...)
 	local v = tonumber(Value.cfgvalue(...))
-	return v or i18n.translate("Zero")
+	return v or translate("Zero")
 end
 
-o = class_s:option(DummyValue, "max_bandwidth", "%s (kbps)" % i18n.translate("Maximum Bandwidth"))
+o = class_s:option(DummyValue, "max_bandwidth", "%s (kbps)" % translate("Maximum Bandwidth"))
 o.cfgvalue = function(...)
 	local v = tonumber(Value.cfgvalue(...))
-	return v or i18n.translate("Unlimited")
+	return v or translate("Unlimited")
 end
 
-o = class_s:option(DummyValue, "minRTT", i18n.translate("Minimize RTT"))
+o = class_s:option(DummyValue, "minRTT", translate("Minimize RTT"))
 o.cfgvalue = function(...)
 	local v = Value.cfgvalue(...)
-	return v and i18n.translate(v) or i18n.translate("No")
+	return v and translate(v) or translate("No")
 end
 
-o = class_s:option(DummyValue, "_ld", "%s (kbps)" % i18n.translate("Load"))
+o = class_s:option(DummyValue, "_ld", "%s (kbps)" % translate("Load"))
 o.rawhtml = true
 o.value   = "<em class=\"ld-download\">*</em>"
 
-rule_s = m:section(TypedSection, "download_rule", i18n.translate("Classification Rules"),
-	i18n.translate("Packets are tested against the rules in the order specified -- rules toward <br />the top have priority. As soon as a packet matches a rule it is classified, <br />and the rest of the rules are ignored. The order of the rules can be altered <br />using the arrow controls.")
+rule_s = m:section(TypedSection, "download_rule", translate("Classification Rules"),
+	translate("Packets are tested against the rules in the order specified -- rules toward <br />the top have priority. As soon as a packet matches a rule it is classified, <br />and the rest of the rules are ignored. The order of the rules can be altered <br />using the arrow controls.")
 	)
 rule_s.addremove = true
 rule_s.sortable  = true
@@ -104,11 +103,11 @@ rule_s.create    = function(e, t)
 	http.redirect(rule_s.extedit:format(t))
 end
 
-o = rule_s:option(ListValue, "class", i18n.translate("Service Class"))
+o = rule_s:option(ListValue, "class", translate("Service Class"))
 for _, s in ipairs(download_classes) do o:value(s.name, s.alias) end
 
-o = rule_s:option(Value, "proto", i18n.translate("Transport Protocol"))
-o:value("", i18n.translate("All"))
+o = rule_s:option(Value, "proto", translate("Transport Protocol"))
+o:value("", translate("All"))
 o:value("tcp", "TCP")
 o:value("udp", "UDP")
 o:value("icmp", "ICMP")
@@ -122,67 +121,67 @@ o.write = function(self, section, value)
 	Value.write(self, section, value:lower())
 end
 
-o = rule_s:option(Value, "family", i18n.translate("Family"))
-o:value("any", i18n.translate("All"))
+o = rule_s:option(Value, "family", translate("Family"))
+o:value("any", translate("All"))
 o:value("ipv4", "ipv4")
 o:value("ipv6", "ipv6")
 o.default = "any"
 
-o = rule_s:option(Value, "source", i18n.translate("Source IP(s)"))
-o:value("", i18n.translate("All"))
+o = rule_s:option(Value, "source", translate("Source IP(s)"))
+o:value("", translate("All"))
 wa.cbi_add_knownips(o)
 o.datatype = "or(ipmask4, ipmask6)"
 
-o = rule_s:option(Value, "srcport", i18n.translate("Source Port(s)"))
-o:value("", i18n.translate("All"))
+o = rule_s:option(Value, "srcport", translate("Source Port(s)"))
+o:value("", translate("All"))
 o.datatype  = "or(port, portrange)"
 
-o = rule_s:option(Value, "destination", i18n.translate("Destination IP(s)"))
-o:value("", i18n.translate("All"))
+o = rule_s:option(Value, "destination", translate("Destination IP(s)"))
+o:value("", translate("All"))
 wa.cbi_add_knownips(o)
 o.datatype = "or(ipmask4, ipmask6)"
 
-o = rule_s:option(Value, "dstport", i18n.translate("Destination Port(s)"))
-o:value("", i18n.translate("All"))
+o = rule_s:option(Value, "dstport", translate("Destination Port(s)"))
+o:value("", translate("All"))
 o.datatype = "or(port, portrange)"
 
-o = rule_s:option(DummyValue, "min_pkt_size", i18n.translate("Minimum Packet Length"))
+o = rule_s:option(DummyValue, "min_pkt_size", translate("Minimum Packet Length"))
 o.cfgvalue = function(...)
 	local v = tonumber(Value.cfgvalue(...))
 	if v and v > 0 then
 		return wa.byte_format(v)
 	end
-	return i18n.translate("Not set")
+	return translate("Not set")
 end
 
-o = rule_s:option(DummyValue, "max_pkt_size", i18n.translate("Maximum Packet Length"))
+o = rule_s:option(DummyValue, "max_pkt_size", translate("Maximum Packet Length"))
 o.cfgvalue = function(...)
 	local v = tonumber(Value.cfgvalue(...))
 	if v and v > 0 then
 		return wa.byte_format(v)
 	end
-	return i18n.translate("Not set")
+	return translate("Not set")
 end
 
-o = rule_s:option(DummyValue, "connbytes_kb", i18n.translate("Connection Bytes Reach"))
+o = rule_s:option(DummyValue, "connbytes_kb", translate("Connection Bytes Reach"))
 o.cfgvalue = function(...)
 	local v = tonumber(Value.cfgvalue(...))
 	if v and v > 0 then
 		return wa.byte_format(v * 1024)
 	end
-	return i18n.translate("Not set")
+	return translate("Not set")
 end
 
 if qos.has_ndpi() then
-	o = rule_s:option(DummyValue, "ndpi", i18n.translate("DPI Protocol"))
+	o = rule_s:option(DummyValue, "ndpi", translate("DPI Protocol"))
 	o.cfgvalue = function(...)
 		local v = Value.cfgvalue(...)
-		return v or i18n.translate("All")
+		return v or translate("All")
 	end
 	
 end
 
-o = rule_s:option(Value, "test_order", i18n.translate("Priority"))
+o = rule_s:option(Value, "test_order", translate("Priority"))
 o.datatype = "range(0, 4194303)"
 
 return m
