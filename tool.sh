a553cdd9a599ed93d898683aefcf2903b867056c
Coolkids <s196774065@gmail.com>
2025-12-06 11:29:49 +0800

    update ci

--- tool.sh
+++ tool.sh
@@ -82,9 +82,9 @@ rm -rf ./feeds/packages/net/mosdns
 rm -rf feeds/packages/net/curl
 git clone https://github.com/sbwml/feeds_packages_net_curl feeds/packages/net/curl
 #smartdns  使用稳定版本
-rm -rf ./feeds/packages/net/smartdns
-rm -rf ./feeds/luci/applications/luci-app-smartdns
-smartdns
+#rm -rf ./feeds/packages/net/smartdns
+#rm -rf ./feeds/luci/applications/luci-app-smartdns
+#smartdns
 ./scripts/feeds install -a
 
 #cp $patchs/patchs/rust/Makefile ./feeds/packages/lang/rust/Makefile
